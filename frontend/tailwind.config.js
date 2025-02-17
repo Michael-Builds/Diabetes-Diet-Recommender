1a8d06aa1cd88f0fc10188ceae30ddefdbfb0d9b
Michael-Builds <michaelkpantramp@gmail.com>
2025-02-17 13:07:21 +0000

    Updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,25 @@ export default {
       fontFamily: {
         geist: ["Geist", "serif"],
       },
+      screens: {
+        xs: "450px",
+        // => @media (min-width: 450px) { ... }
+
+        sm: "575px",
+        // => @media (min-width: 576px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "992px",
+        // => @media (min-width: 992px) { ... }
+
+        xl: "1200px",
+        // => @media (min-width: 1200px) { ... }
+
+        "2xl": "1400px",
+        // => @media (min-width: 1400px) { ... }
+      },
     },
   },
   plugins: [require("@tailwindcss/line-clamp")],
