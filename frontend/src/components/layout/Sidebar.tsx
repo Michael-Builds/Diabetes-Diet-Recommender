703c0b2a172389e74c218f5707b55f5c7d054784
Michael-Builds <michaelkpantramp@gmail.com>
2025-02-17 13:14:13 +0000

    Updated

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
     return (
-        <section className="bg-[#ffffff] font-geist shadow-sm h-full rounded-lg py-3 w-[15rem] px-5 items-center gap-20 flex flex-col">
+        <section className="bg-[#ffffff] font-geist shadow-sm h-full rounded-lg py-3 w-full px-5 items-center gap-20 flex flex-col">
             <div>Logo</div>
             <div>Nav items</div>
         </section>
